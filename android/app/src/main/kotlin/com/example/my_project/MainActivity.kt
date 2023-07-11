3d4b29fc3cf0c649c20bcc44965face5a24d43a6
shahbajjamil <shahbajjamil2014@gmail.com>
2023-07-11 23:34:04 +0530

    init screen bug fix

--- android/app/src/main/kotlin/com/example/my_project/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/my_project/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.mycompany.sphara
+package app.sphara.sphara
 
 import io.flutter.embedding.android.FlutterActivity
 
--- lib/flutter_flow/nav/nav.dart
+++ lib/flutter_flow/nav/nav.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:go_router/go_router.dart';
 import 'package:page_transition/page_transition.dart';
 import '../flutter_flow_theme.dart';
@@ -1067,7 +1068,7 @@ class FFRoute {
           final child = appStateNotifier.loading
               ? Container(
                   color: Colors.transparent,
-                  child: Image.asset(
+                  child: SvgPicture.asset(
                     'assets/images/bg.svg',
                     fit: BoxFit.cover,
                   ),
